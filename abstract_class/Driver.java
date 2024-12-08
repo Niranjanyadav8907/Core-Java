ddd2fd1bbecdb16b1917d9a2429c9405a257a867
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-12-08 22:23:58 +0530

    Add files via upload

---
+++ abstract_class/Driver.java
@@ -0,0 +1,12 @@
+package abstract_class;
+
+public class Driver {
+	public static void main(String[] args) {
+		Vehicle v1=new ElectricCar();
+		v1.start();
+		v1.stop();
+		v1.drive();
+		
+	}
+
+}
---
+++ abstract_class/Vehicle.java
@@ -0,0 +1,25 @@
+package abstract_class;
+
+public abstract class Vehicle {
+	static int x=12;
+	int y;
+	static
+	{
+		System.out.println("static block");
+	}
+	{
+		System.out.println("NOn static block");
+	}
+	
+		public abstract void start();
+		public abstract void stop();
+		
+		public void drive() {
+			System.out.println("Drive vehicle");
+		}
+		public void accelerate() {
+			System.out.println("Accelerate vehicle");
+		}
+	
+
+}
